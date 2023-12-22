4444e4013b6a742c28f73ab54c9ce0fe7ab7ffe6
tam-dcreator <tam@THE-7TH>
2023-12-22 18:09:32 +0100

    Created a puppet manifest to create a file in /tmp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# This source file creates a file in /tmp
+
+file { '0-create_a_file.pp':
+  path    => '/tmp/school',
+  mode    => '0744'
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
