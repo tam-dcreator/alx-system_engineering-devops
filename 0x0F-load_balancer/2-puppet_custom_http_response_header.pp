ad60ff340e4a4ee4197cdfb8813d5bfa81ffbcaa
tam-dcreator <tam@THE-7TH>
2024-07-26 18:52:23 +0100

    New puppet manifest

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,31 +1,25 @@
 # Puppet configuration for custom HTTP header
 
-# Update system and install nginx
 exec {'update':
-  command => 'sudo apt-get update && sudo apt-get -y install nginx',
-  path    => '/usr/bin',
-}
-
-# Ensure Nginx is present
-package { 'nginx':
-  ensure => 'present',
-}
-
-# Set Homepage content
-exec {'Hello':
-  command  => 'echo "Hello World!" | tee /var/www/html/index.html',
   provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
 }
 
-# Set custom header
-exec {'add_header':
-  environment => ["HOST=${hostname}"],
-  command     => 'sudo sed -i "s/server_name _;/server_name _;\\n\\tadd_header X-Served-By $HOST;\\n/" /etc/nginx/sites-enabled/default',
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
+}
+
+exec { 'add_header':
   provider    => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
 }
 
-# Restart Nginx
-exec {'run':
-  command  => 'sudo service nginx restart',
+exec { 'restart Nginx':
   provider => shell,
+  command  => 'sudo service nginx restart',
 }
