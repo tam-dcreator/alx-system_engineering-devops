18c227acdb609d2dda69e3b6e713d9967e917982
tam-dcreator <tam@THE-7TH>
2024-07-26 18:51:05 +0100

    Fixed semantics

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,8 +2,8 @@
 
 # Update system and install nginx
 exec {'update':
-  command=>'sudo apt-get update && sudo apt-get -y install nginx',
-  path => '/usr/bin',
+  command => 'sudo apt-get update && sudo apt-get -y install nginx',
+  path    => '/usr/bin',
 }
 
 # Ensure Nginx is present
@@ -20,8 +20,8 @@ exec {'Hello':
 # Set custom header
 exec {'add_header':
   environment => ["HOST=${hostname}"],
-  command => 'sudo sed -i "s/server_name _;/server_name _;\\n\\tadd_header X-Served-By $HOST;\\n/" /etc/nginx/sites-enabled/default',
-  provider => shell,
+  command     => 'sudo sed -i "s/server_name _;/server_name _;\\n\\tadd_header X-Served-By $HOST;\\n/" /etc/nginx/sites-enabled/default',
+  provider    => shell,
 }
 
 # Restart Nginx
